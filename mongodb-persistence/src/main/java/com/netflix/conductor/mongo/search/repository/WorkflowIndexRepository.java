91eeead3817f21f86691a6e9677f70a3ac32b570
Sudhir Yelikar <sudhir.yelikar@thoughtworks.com>
2023-09-08 23:28:50 +0530

    Remove old code

--- mongodb-persistence/src/main/java/com/netflix/conductor/mongo/search/repository/WorkflowIndexRepository.java
+++
@@ -1,8 +0,0 @@
-package com.netflix.conductor.mongo.search.repository;
-
-import com.netflix.conductor.mongo.search.models.WorkflowIndex;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface WorkflowIndexRepository extends MongoRepository<WorkflowIndex, String> {
-
-}
